d462565a389d099e7b306e69c2d18ba23d28b75b
shero <bktotient@gmail.com>
2025-08-20 14:45:10 -0400

    remove recommendation tool

--- src/agents/tools/__init__.py
+++ src/agents/tools/__init__.py
@@ -1,4 +1,3 @@
 from .preferences import update_user_preferences
-from .recommendations import get_listing_recommendations
 
-tools = [get_listing_recommendations, update_user_preferences]
+tools = [update_user_preferences]
